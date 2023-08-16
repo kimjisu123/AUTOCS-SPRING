70f5a7a57da44334e5bf53d6d382125f4352dcc5
KimHaedeun <eun06151@naver.com>
2023-08-16 17:19:21 +0900

    Initial commit

---
+++ src/main/java/com/css/autocsfinal/AutocsFinalApplication.java
@@ -0,0 +1,13 @@
+package com.css.autocsfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocsFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutocsFinalApplication.class, args);
+    }
+
+}
