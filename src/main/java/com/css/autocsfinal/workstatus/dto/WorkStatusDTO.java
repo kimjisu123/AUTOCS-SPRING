bf47a75a73db1c4b6fb2817f2b194fc931034c0c
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2023-08-21 22:33:57 +0900

    오류 수정

--- src/main/java/com/css/autocsfinal/workstatus/dto/WorkStatusDTO.java
+++ src/main/java/com/css/autocsfinal/workstatus/dto/WorkStatusDTO.java
@@ -1,8 +1,35 @@
 package com.css.autocsfinal.workstatus.dto;
 
+import lombok.*;
 import org.springframework.http.HttpStatus;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class WorkStatusDTO {
+
+    private int workStatusCode;
+
+    private Date attendanceTime;
+
+    private Date quittingTime;
+
+    private Date totalHoursDuty;
+
+    private char vactionStatus;
+
+    private char absenceWorkStatus;
+
+    private Date workingDate;
+
     public WorkStatusDTO(HttpStatus httpStatus, String 조회_성공, Object o) {
     }
 }
