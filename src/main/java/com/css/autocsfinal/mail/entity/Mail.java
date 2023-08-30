e6857dd670a895a1ba1ef41eff873068d0053586
JISU <dkljjh10@gmail.com>
2023-08-30 10:22:52 +0900

    Revert "2023-08-29 commit"
    This reverts commit 831071d5

---
+++ src/main/java/com/css/autocsfinal/mail/entity/Mail.java
@@ -0,0 +1,50 @@
+package com.css.autocsfinal.mail.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "TBL_MAIL")
+@SequenceGenerator(
+        name = "MAIL_NO",
+        sequenceName = "SEQ_MAIL_NO",
+        initialValue = 1,
+        allocationSize = 1
+)
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mail {
+
+    @Id
+    @Column(name ="MAIL_NO")
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "MAIL_NO"
+    )
+    private int mailNo;
+
+    @Column(name = "SEND")
+    private String send;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "CONTEXT")
+    private String context;
+
+    @Column(name = "GO_DATE")
+    private Date goDate;
+
+    @Column(name = "STATUS")
+    private String status;
+
+//    @OneToMany(mappedBy = "mail")
+//    @Column(name ="MAIL_NO")
+//    private List<MailList> mailList;
+}
---
+++ src/main/java/com/css/autocsfinal/mail/service/MailService.java
@@ -0,0 +1,90 @@
+package com.css.autocsfinal.mail.service;
+
+import com.css.autocsfinal.mail.dto.MailDTO;
+import com.css.autocsfinal.mail.dto.MailListDTO;
+import com.css.autocsfinal.mail.entity.Mail;
+//import com.css.autocsfinal.mail.entity.MailList;
+//import com.css.autocsfinal.mail.repository.MailListRepository;
+import com.css.autocsfinal.mail.repository.MailRepository;
+import com.css.autocsfinal.member.entity.Employee;
+import com.css.autocsfinal.member.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class MailService {
+
+    private final MailRepository mailRepository;
+    private final EmployeeRepository employeeRepository;
+    private final ModelMapper modelMapper;
+
+    public List<MailDTO> findMail() {
+
+        List<Mail> mailList = mailRepository.findAll();
+
+        List<MailDTO> mailDTOList = mailList.stream().map(Mail -> modelMapper.map(Mail, MailDTO.class) ).collect(Collectors.toList());
+
+        return mailDTOList;
+    }
+
+    public Object mailBookmark() {
+
+        List<Mail> mailList = mailRepository.findByStatus("Y");
+        log.info("============================================================= mailDTOList" + mailList);
+
+        List<MailDTO> mailDTOList = mailList.stream().map(mail -> modelMapper.map(mail, MailDTO.class)).collect(Collectors.toList());
+
+        log.info("============================================================= mailDTOList" + mailDTOList);
+
+        return mailDTOList;
+
+    }
+
+    public Object saveMail(MailDTO mailDTO1) {
+
+
+        Mail mail = modelMapper.map(mailDTO1, Mail.class);
+        mailRepository.save(mail);
+
+        return null;
+
+    }
+
+    public Object deleteMail() {
+
+        mailRepository.deleteAll();
+
+        return null;
+    }
+
+    @Transactional
+    public Object setMail(MailDTO mailDTO) {
+
+        int mailNo = mailDTO.getMailNo();
+
+        Mail mail = mailRepository.findById(mailNo).get();
+
+        if(mail.getStatus().equals("N")){
+            mail.setStatus("Y");
+        } else {
+            mail.setStatus("N");
+        }
+
+        MailDTO resultMail = modelMapper.map(mail, MailDTO.class);
+
+        return resultMail;
+
+    }
+
+
+
+
+}
