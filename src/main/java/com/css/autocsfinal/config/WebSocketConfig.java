55567e3f8c1e55d545f8244857e194d7d4be8787
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2023-11-04 23:50:39 +0900

    2023-11-04 commit 웹소켓 기능 구현중 No Handler 에러 수정 및 추가로 웹소켓 학습중 원시 웹소켓과 STOMP를 사용하는 웹소켓의 차이점 및 STOMP를 사용 했을 때 장점 그리고 STOMP와 원시타입의 코드 구성과 흐름 이해

--- src/main/java/com/css/autocsfinal/config/WebSocketConfig.java
+++ src/main/java/com/css/autocsfinal/config/WebSocketConfig.java
@@ -7,12 +7,8 @@ import org.springframework.web.socket.config.annotation.*;
 
 /** WebSocket 엔드포인트 및 메시지 처리기를 설정합니다. */
 @Configuration
-@EnableWebSocketMessageBroker
 @EnableWebSocket
-@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer{
-
-    private final MailWebSocketHandler milWebSocketHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
@@ -20,8 +16,8 @@ public class WebSocketConfig implements WebSocketConfigurer{
 
         // WebSocketHandler 클래스를 WebSocket 핸들러로 등록하고 "/mail-ws" 엔드포인트 경로 매핑
         // 클라이언트는 WebSocket 연결을 "/mail-ws" 엔드포인트로 생성하여 이 핸들러를 사용할 수 있다.
-        registry.addHandler(milWebSocketHandler, "/mail-ws"); // 1번째 인자에는 웹소켓 핸들러, 2번째 인자에는 엔드포인트 경로
+        registry.addHandler(new MailWebSocketHandler(), "/mail-ws") // 1번째 인자에는 웹소켓 핸들러, 2번째 인자에는 엔드포인트 경로
+                .setAllowedOrigins("*") // cors same-origin만 허용
+                .withSockJS();          // SockJS 사용하도록 설정
     }
-
-
 }
--- src/main/java/com/css/autocsfinal/webSocket/MailWebSocketHandler.java
+++ src/main/java/com/css/autocsfinal/webSocket/MailWebSocketHandler.java
@@ -2,7 +2,6 @@ package com.css.autocsfinal.webSocket;
 
 import com.css.autocsfinal.mail.entity.Mail;
 import com.css.autocsfinal.mail.repository.MailRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -11,31 +10,10 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.util.List;
 
-@Component
 public class MailWebSocketHandler extends TextWebSocketHandler {
-
-    private final SimpMessagingTemplate messagingTemplate;
-    private final MailRepository mailRepository;
-
-    public MailWebSocketHandler(SimpMessagingTemplate messagingTemplate, MailRepository mailRepository) {
-        this.messagingTemplate = messagingTemplate;
-        this.mailRepository = mailRepository;
-    }
-
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         // WebSocket 클라이언트로부터의 메시지 처리
-    }
-
-    public void sendUnreadMailNotification() {
-
-        List<Mail> unreadMails = mailRepository.findByRead("N");
-
-        if (!unreadMails.isEmpty()) {
-            String notificationMessage = "읽지 않은 메일의 수는 " + unreadMails.size() + "개 입니다.";
-            messagingTemplate.convertAndSend("/topic/mail-notifications", notificationMessage);
-        }
-
     }
 }
